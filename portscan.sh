986d0982b492aa54ae8a6c97a8adc9dd382db1b1
Kathan Patel <patelkathan22@gmail.com>
2020-06-18 10:41:59 +0530

    Update portscan.sh

--- portscan.sh
+++ portscan.sh
@@ -36,7 +36,7 @@ mass_scan(){
 echo -e "\n\e[36m[\e[32m+\e[36m]\e[92m USING MASSNMAP TO SCAN SUBDOMAINS\e[0m\n";
 cat $target | while read subs; do echo $PASS | sudo -S ./tools/massnmap.sh $subs ;done
 sudo mv massnmap/ ./results/$dir/
-rm -rf ./results/$dir/scan_results
+rmdir ./results/$dir/scan_results
 echo -e "\nPORT SCANNING COMPLETED \e[32mFINISH\e[0m";
 }
 
@@ -44,14 +44,14 @@ shodanfy_scan(){
 echo -e "\n\e[36m[\e[32m+\e[36m]\e[92m USING Shodanfy.py FOR PORT-SCAN\e[0m\n";
 cat $target | filter-resolved | cf-check > ./results/$dir/ip_to_scan.txt
 cat ./results/$dir/ip_to_scan.txt | python3 ./tools/Shodanfy.py/shodanfy.py --stdin --getvuln --getinfo --getports | tee ./results/$dir/port_scan_results.txt
-rm -rf ./results/$dir/scan_results
+rmdir ./results/$dir/scan_results
 echo -e "\nPORT SCANNING COMPLETED \e[32mFINISH\e[0m";
 }
 
 simple_nmap(){
 echo -e "\n\e[36m[\e[32m+\e[36m]\e[92m USING NMAP TO SCAN SUBDOMAINS\e[0m\n";
 nmap -sV -T3 -Pn -p2075,2076,6443,3868,3366,8443,8080,9443,9091,3000,8000,5900,8081,6000,10000,8181,3306,5000,4000,8888,5432,15672,9999,161,4044,7077,4040,9000,8089,443,7447,7080,8880,8983,5673,7443,19000,19080,9200 -iL $target | tee ./results/$dir/nmap_raw_scan.txt
-rm -rf ./results/$dir/scan_results
+rmdir ./results/$dir/scan_results
 echo -e "\nPORT SCANNING COMPLETED \e[32mFINISH\e[0m";
 }
 
